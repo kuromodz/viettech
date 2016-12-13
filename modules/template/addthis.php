b4b3cb9c39e850ee63385758c4d6f35b9ad4fcbf
Kuro Virus <kuromodz@gmail.com>
2016-12-13 15:00:36 +0700

    Clear temp

--- modules/template/addthis.php
+++ modules/template/addthis.php
@@ -1,22 +1,16 @@
-<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
-    <a class="addthis_button_facebook"></a>
-    <a class="addthis_button_twitter"></a>
-    <a class="addthis_button_google_plusone_share"></a>
-    <a class="addthis_button_youtube"></a>
-    <a class="addthis_button_linkedin"></a>
-    <a class="addthis_button_print"></a>
-    <a class="addthis_button_email"></a>
-</div>
-<script>
-    if (window.addthis) {
-        window.addthis = null;
-        window._adr = null;
-        window._atc = null;
-        window._atd = null;
-        window._ate = null;
-        window._atr = null;
-        window._atw = null;
-    }
-    $.getScript('admin/plugins/addthis/addthis.js');    
-</script>
-<br>
+<a target="_blank" class="btn btn-default button_facebook" href="https://www.facebook.com/dialog/share?app_id=140586622674265&display=popup&href=<?=pageUrl()?>"><i class="fa fa-facebook fa-fw"></i></a>
+<a target="_blank" class="btn btn-default button_twitter" href="http://www.twitter.com/share?url=<?=pageUrl()?>"><i class="fa fa-twitter fa-fw"></i> </a>
+<a target="_blank" class="btn btn-default button_google_plusone_share" href="https://plus.google.com/share?url<?=pageUrl()?>"><i class="fa fa-google-plus fa-fw"></i> </a>
+<a target="_blank" class="btn btn-default button_youtube" href="<?=pageUrl()?>"><i class="fa fa-youtube fa-fw"></i> </a>
+<a target="_blank" class="btn btn-default button_linkedin" href="<?=pageUrl()?>"><i class="fa fa-linkedin fa-fw"></i> </a>
+<a target="_blank" class="btn btn-default button_email" href="mailto:?&body=<?=pageUrl()?>"><i class="fa fa-envelope fa-fw"></i> </a>
+
+<style type="text/css">
+    .btn.button_facebook{background-color:rgb(59, 89, 152);}
+    .btn.button_twitter{background-color:rgb(29, 161, 242);}
+    .btn.button_google_plusone_share{background-color:rgb(220, 78, 65);}
+    .btn.button_youtube{background-color:rgb(205, 32, 31);}
+    .btn.button_linkedin{background-color:rgb(0, 119, 181);}
+    .btn.button_print{background-color:rgb(115, 138, 141);}
+    .btn.button_email{background-color:rgb(132, 132, 132);}
+</style>
--- modules/template/asset.php
+++ modules/template/asset.php
@@ -67,7 +67,7 @@
           var js, fjs = d.getElementsByTagName(s)[0];
           if (d.getElementById(id)) return;
           js = d.createElement(s); js.id = id;
-          js.src = "admin/assets/js/sdk.js";
+          js.src = "//connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v2.6&appId=174220409684186";
           fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
 
