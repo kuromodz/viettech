dd6951860d98eea00c45e1ffbfc112afb54c135e
Kuro Virus <kuromodz@gmail.com>
2016-12-13 15:04:31 +0700

    Fix addthis

--- modules/template/addthis.php
+++ modules/template/addthis.php
@@ -1,6 +1,6 @@
 <a target="_blank" class="btn btn-default button_facebook" href="https://www.facebook.com/dialog/share?app_id=140586622674265&display=popup&href=<?=pageUrl()?>"><i class="fa fa-facebook fa-fw"></i></a>
 <a target="_blank" class="btn btn-default button_twitter" href="http://www.twitter.com/share?url=<?=pageUrl()?>"><i class="fa fa-twitter fa-fw"></i> </a>
-<a target="_blank" class="btn btn-default button_google_plusone_share" href="https://plus.google.com/share?url<?=pageUrl()?>"><i class="fa fa-google-plus fa-fw"></i> </a>
+<a target="_blank" class="btn btn-default button_google_plusone_share" href="https://plus.google.com/share?url=<?=pageUrl()?>"><i class="fa fa-google-plus fa-fw"></i> </a>
 <a target="_blank" class="btn btn-default button_youtube" href="<?=pageUrl()?>"><i class="fa fa-youtube fa-fw"></i> </a>
 <a target="_blank" class="btn btn-default button_linkedin" href="<?=pageUrl()?>"><i class="fa fa-linkedin fa-fw"></i> </a>
 <a target="_blank" class="btn btn-default button_email" href="mailto:?&body=<?=pageUrl()?>"><i class="fa fa-envelope fa-fw"></i> </a>
